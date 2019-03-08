a21d54bb355e9d175420cf9c393711fc4c1673eb
ThomasLindo <thomas.lindo8275@gmail.com>
2019-03-07 19:50:51 -0500

    Update
    + raycasting

---
+++ leapmotion testing ground/Assets/RaycastScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class RaycastScript : MonoBehaviour
+{
+    public GameObject PalmTracker;
+
+
+    private void Update()
+    {
+        RaycastHit hit;
+        Ray raycast = new Ray(transform.position, transform.forward);
+        if (Physics.Raycast(raycast, out hit, 100000000.0f))
+        {
+            if (hit.transform != null)
+            {
+                if (hit.transform.tag == "Button")
+                {
+                    Debug.Log("that's a button");
+                    hit.transform.GetComponent<Button>().onClick.Invoke();
+                }
+            }
+        }
+    }
+}
---
+++ leapmotion testing ground/Assets/UIButton.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+//put this script on any buttons you want to be clickable in vr!
+//don't forget to give your button the "button" tag as well!!!
+[RequireComponent(typeof(RectTransform))]
+public class UIButton : MonoBehaviour
+{
+    private BoxCollider boxCollider;
+    private RectTransform rectTransform;
+
+    private void OnEnable()
+    {
+        ValidateCollider();
+    }
+
+    private void OnValidate()
+    {
+        ValidateCollider();
+    }
+
+    private void ValidateCollider()
+    {
+        rectTransform = GetComponent<RectTransform>();
+
+        boxCollider = GetComponent<BoxCollider>();
+        if (boxCollider == null)
+        {
+            boxCollider = gameObject.AddComponent<BoxCollider>();
+        }
+
+        boxCollider.size = rectTransform.sizeDelta;
+    }
+}
